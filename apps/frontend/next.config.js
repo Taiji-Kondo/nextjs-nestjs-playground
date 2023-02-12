1f361bbe047b748218a2f9a1db68107bee3d98f3
taiji <k.taiji1108@gmail.com>
2023-02-12 20:45:01 +0900

    chore: Optimize image and font

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts'],
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  optimizeFonts: true,
 }
 
 module.exports = nextConfig
